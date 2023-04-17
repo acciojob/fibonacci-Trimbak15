425d14bd1b3b96ba3cdbb97cfe8679a7a59d964e
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-17 13:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 function fibonacci(n) {
-  let a = 0;
-  let b = 1;
-  let temp;
-
-  if (n === 0) {
-    return a;
+  if (n <= 1) {
+    return n;
   }
-
-  for (let i = 2; i <= n; i++) {
-    temp = a + b;
-    a = b;
-    b = temp;
-  }
-
-  return b;
+  return fibonacci(n-1) + fibonacci(n-2);
 }
 
 
