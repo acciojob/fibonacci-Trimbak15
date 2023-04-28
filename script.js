38a5c7fd8b785acf203f021ae9fe7288fbfa98d8
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-28 12:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function fibonacci(num) {
-  if (num <= 1) {
-    return num;
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
-  return fibonacci(num-1) + fibonacci(num-2);
 }
 
-
-module.exports = fibonacci;
